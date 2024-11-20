938b231ea0f195e3b2083b623f0bf39096cef5fa
Kamilovkz <001kamilov@gmail.com>
2024-11-20 20:15:27 +0500

    nothing new, just small changes

--- swh.py
+++ swh.py
@@ -1,10 +1,10 @@
 import requests
 from config.settings import settings
 
-whook = "https://57d877d3351739.lhr.life/"
+whook = "https://7eb660a6c4273e.lhr.life"
 
 r = requests.get(
-    f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_KEY}/setWebhook?url={whook}"
+    f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_KEY}/setWebhook?url={whook}/"
 )
 
 print(r.json())
